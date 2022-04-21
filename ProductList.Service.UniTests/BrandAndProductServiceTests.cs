9fdd7eec921154c5633aa3512f01c5b2969f05d2
utkarshsimform <utkarsh.p@simformsolutions.com>
2022-04-21 17:57:42 +0530

    Project name update

---
+++ ProductList.Service.UniTests/BrandAndProductServiceTests.cs
@@ -0,0 +1,149 @@
+﻿using ProtoType.Model;
+using ProtoType.Model.DatabaseEntity;
+using ProtoType.Repository.Interfaces;
+using ProtoType.Service.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using Illustration.Service.UnitTests;
+
+namespace ProtoType.Service.UnitTests
+{
+    [TestFixture]
+    public class BrandAndProductServiceTests
+    {
+        private Mock<IBrandAndProductRepository> _brandAndProductRepository;
+        private BrandAndProductService _brandAndProductService;
+        private Mock<IContextAccessorService> _contextAccessorService;                
+        private Guid _mockedTenantId;
+        private string _mockedInstanceId;
+        private Mock<IProtoTypeContext> _iProtoTypeContext;        
+        private Mock<IIdentity> _identity;
+
+        [SetUp]
+        public void Setup()
+        {
+            _brandAndProductRepository = new();
+            _contextAccessorService = new();            
+            _identity = new();
+
+            _mockedInstanceId = "localhost";
+            _mockedTenantId = Guid.NewGuid();            
+
+            var options = new DbContextOptionsBuilder<IProtoTypeContext>().UseInMemoryDatabase(databaseName: "ProtoType").Options;
+            _iProtoTypeContext = new(options);
+
+            _brandAndProductService = new(                
+                _brandAndProductRepository.Object,                 
+                _iProtoTypeContext.Object                
+                );            
+        }
+
+        [Test]
+        public async Task AddBrandAndProductAsyncTest()
+        {
+            // Arrange
+            List<Model.Models.BrandAndProduct> brandAndProducts = new()
+            {
+                new()
+                {
+                    Id = 0,
+                    BrandId = 1,
+                    BrandName = "Mock Brand Name",
+                    CredentialId = 1,
+                    ProductName = "Mock Product Name"
+                }
+            };
+            _brandAndProductRepository.Setup(r => r.SaveBrandAndProductAsync(It.IsAny<BrandAndProduct>())).Returns(Task.FromResult(1));
+
+            // Act
+            var insertedIds = await _brandAndProductService.AddBrandAndProductAsync(brandAndProducts);
+
+            // Assert
+            Assert.IsNotNull(insertedIds);
+        }
+
+        [Test]
+        public void DeleteBrandAndProductByIdTest()
+        {
+            // Arrange
+            var getByIdOutput = new BrandAndProduct() { Id = 1 };
+            _brandAndProductRepository.Setup(s => s.GetById(1)).Returns(getByIdOutput);
+            _brandAndProductRepository.Setup(r => r.Remove(getByIdOutput)).Verifiable();
+
+            // Act and verify if method is called or not.
+            _brandAndProductService.DeleteBrandAndProductById(1);
+        }
+
+        [Test]
+        public void DeleteBrandAndProductByIdInvalidOperationExceptionTest()
+        {
+            // Arrange
+            var getByIdOutput = new BrandAndProduct() { Id = 1 };
+            _brandAndProductRepository.Setup(s => s.GetById(0)).Returns((BrandAndProduct)null);
+
+            // Act
+            var result = _brandAndProductService.DeleteBrandAndProductById(1);
+
+            //Assert
+            Assert.IsTrue(result == null);
+        }
+
+        [Test]
+        public async Task GetBrandAndProductTestAsync()
+        {
+            // Arrange
+            List<BrandAndProduct> repositoryOutput = new()
+            {
+                new()
+                {
+                    Id = 1,
+                    BrandId = 1,
+                    BrandName = "Mock Brand Name",
+                    CredentialId = 1,
+                    ProductName = "Mock Product Name"
+                }
+            };
+
+            _iProtoTypeContext.Object.BrandAndProducts = ServiceTestsHelper.GetQueryableMockDbSet(repositoryOutput);
+
+            // Act
+            var brandAndProductId = await _brandAndProductService.GetBrandAndProductsAsync();            
+        }
+
+        [Test]
+        public async Task UpdateBrandAndProductTest()
+        {
+            //Arrange
+            Model.Models.BrandAndProduct brandAndProductModel = new Model.Models.BrandAndProduct
+            {
+                Id = 1,
+                BrandId = 1,
+                BrandName = "Mock Brand Name",
+                CredentialId = 1,
+                ProductName = "Mock Product Name"
+            };
+
+            var brandAndProduct = new BrandAndProduct()
+            {
+                Id = 1,
+                BrandId = 1,
+                BrandName = "Mock Brand Name",
+                CredentialId = 1,
+                ProductName = "Mock Product Name"
+            };
+            _brandAndProductRepository.Setup(s => s.GetById(1)).Returns(brandAndProduct);
+
+            //Act
+            var result = await _brandAndProductService.UpdateBrandAndProductAsync(brandAndProduct.Id, brandAndProductModel);
+
+            // Assert
+            Assert.AreEqual(brandAndProduct.Id, result);
+        }
+    }
+}
---
+++ ProductList.Service.UniTests/ServiceTestsHelper.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNet.OData;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.AspNet.OData.Query;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Illustration.Service.UnitTests
+{
+    public static class ServiceTestsHelper
+    {
+        /// <summary>
+        /// Gets an <see cref="ODataQueryOptions"/>
+        /// </summary>
+        /// <seealso cref="https://stackoverflow.com/a/49678474/117127"/>
+        /// <seealso cref="https://github.com/OData/WebApi/issues/1645"/>
+        public static ODataQueryOptions<T> GetODataQueryOptions<T>(String queryString = null) where T : class
+        {
+            var collection = new ServiceCollection();
+
+            collection.AddOData();
+
+            var provider = collection.BuildServiceProvider();
+
+            var routeBuilder = new RouteBuilder(Mock.Of<IApplicationBuilder>(x => x.ApplicationServices == provider));
+            routeBuilder.EnableDependencyInjection();
+
+            var modelBuilder = new ODataConventionModelBuilder(provider);
+            modelBuilder.EntitySet<T>("MyType");
+            var model = modelBuilder.GetEdmModel();
+
+            var httpContext = new DefaultHttpContext
+            {
+                RequestServices = provider
+            };
+            if (queryString != null)
+            {
+                httpContext.Request.QueryString = new QueryString(queryString);
+            }
+
+            var context = new ODataQueryContext(model, typeof(T), new Microsoft.AspNet.OData.Routing.ODataPath());
+            return new ODataQueryOptions<T>(context, httpContext.Request);
+        }
+
+        public static DbSet<T> GetQueryableMockDbSet<T>(List<T> sourceList) where T : class
+        {
+            var queryable = sourceList.AsQueryable();
+
+            var dbSet = new Mock<DbSet<T>>();
+            dbSet.As<IQueryable<T>>().Setup(m => m.Provider).Returns(queryable.Provider);
+            dbSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(queryable.Expression);
+            dbSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(queryable.ElementType);
+            dbSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(() => queryable.GetEnumerator());
+            dbSet.Setup(d => d.Add(It.IsAny<T>())).Callback<T>((s) => sourceList.Add(s));
+
+            return dbSet.Object;
+        }
+    }
+}
